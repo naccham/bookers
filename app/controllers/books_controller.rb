d557e958138a526104d52c815d744b2da68e39d9
EC2 Default User <ec2-user@ip-172-31-27-68.ec2.internal>
2021-12-21 09:42:25 +0000

    [Update]空欄で投稿した時のみに表示されるcss?で詰まっている

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,42 @@
 class BooksController < ApplicationController
 
 
-  def show
-  end
-
-
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books'
-  end
 
   def index
     @book = Book.new
     @books = Book.all
+  end
 
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:complete] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def show
+     @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:complete] = "Book was successfully updated."
+    redirect_to book_path(book)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
